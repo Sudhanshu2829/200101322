cf0dabc4d3e1e8f398832ad9fe4220b50286fe7a
Sudhanshu Kumar <84763877+Sudhanshu2829@users.noreply.github.com>
2023-07-28 09:49:00 +0530

    Update App.js

--- Q2-Number Management Service Application/App.js
+++ Q2-Number Management Service Application/App.js
@@ -1,31 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+const express = require("express");
+const fetch = require("node-fetch");
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React 
-        </a>
-          <center>
-        <a href="http://20.244.56.144/numbers/primes" target="_blank">Prime</a>
-        <a href="http://20.244.56.144/numbers/fibo" target="_blank">Fibo</a>
-        <a href="http://20.244.56.144/numbers/odd" target="_blank">Odd</a>
-        <a href="http://20.244.56.144/numbers/rand" target="_blank">Rand</a>
-    </center>
-      </header>
-    </div>
+const app = express();
+const port = 3000;
+
+// Function to fetch and merge data from multiple URLs
+async function fetchDataFromURLs(urls) {
+  const promises = urls.map((url) =>
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => data.numbers)
+      .catch((err) => [])
   );
+
+  const results = await Promise.allSettled(promises);
+
+  const mergedNumbers = results.reduce((merged, result) => {
+    if (result.status === "fulfilled" && Array.isArray(result.value)) {
+      merged = merged.concat(result.value);
+    }
+    return merged;
+  }, []);
+
+  return Array.from(new Set(mergedNumbers)).sort((a, b) => a - b);
 }
 
-export default App;
+// API endpoint for /numbers
+app.get("/numbers", async (req, res) => {
+  const { url } = req.query;
+
+  if (!url) {
+    return res.status(400).json({ error: "No URLs provided" });
+  }
+
+  const urls = Array.isArray(url) ? url : [url];
+
+  try {
+    const mergedNumbers = await fetchDataFromURLs(urls);
+    return res.json({ numbers: mergedNumbers });
+  } catch (err) {
+    return res.status(500).json({ error: "Error fetching data" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Number Management HTTP microservice listening on port ${port}!`);
+});
